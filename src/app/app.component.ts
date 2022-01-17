fe8ef6532fffeec3709f2ca2183bd088514d7190
hollmanPinto <hollman.pinto07@gmail.com>
2022-01-16 22:14:46 -0500

    ya funcionan los 4 metodos con problemas al cerrar el modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CompaniesService } from './services/companies.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -14,18 +15,30 @@ export class AppComponent implements OnInit{
   closeResult:string='';
   validateButton:string='Status';
   btnStyle:string=`${"btn btn-outline-info"}`
+  datasToBD:object={}
+
+  //insert company variables
+  nameCompanyNew:string='';
+  nitCompanyNew:string='';
+  bussinessNameCompanyNew:string='';
+  applicationDateNew:string='';
+  statusNew:boolean=false;
+  attendantNew:string='';
+
+  //update compaby variables
+  IdCompany:string='';
 
   constructor(
     private companyService:CompaniesService, 
-    public modal:NgbModal
+    public modal:NgbModal,
+    private http:HttpClient
     ){ }
 
   ngOnInit(): void{
     console.log('component initialize');
     this.companyService.getCompanies()
-      .subscribe((response :any ) => this.companyList = response);
+      .subscribe((response :any ) => this.companyList = response);  
   }
-
   incrementCompany(){
     this.index==this.companyList.length-1?this.index=0:this.index+=1
   }
@@ -35,12 +48,62 @@ export class AppComponent implements OnInit{
   statusValidateButton(){
     this.validateButton='Validate!'
     this.btnStyle=`${"btn btn-outline-success"}`
+    this.statusNew=true;
   }
   statusDeclineButton(){
     this.validateButton='Decline!'
     this.btnStyle=`${"btn btn-outline-danger"}`
+    this.statusNew=false;
   }
   deleteAlert() {
-    alert('You going to Delete this Company, Sure?');
+    const confirmed=window.confirm('Are you Sure delete this Company?');
+    if(confirmed){
+      this.IdCompany=this.companyList[this.index].url;
+      this.companyService.deleteCompanies(this.IdCompany).subscribe((result)=>{
+      console.warn(this.IdCompany)
+      window.location.reload();
+      
+      })
+    }
+    
+  }
+
+  //Function for insert additional companies in DB
+  insertCompany(nameNew:string,nitNew:string,bussinessNew:string,dateNew:string,attendantNew:string){
+    this.datasToBD={
+      "Name":nameNew,
+      "NIT":nitNew,
+      "businessName":bussinessNew,
+      "applicationDate":dateNew,
+      "status":this.statusNew,
+      "attendant":attendantNew
+    }
+    console.log(this.datasToBD)
+
+    this.companyService.addCompanies(this.datasToBD).subscribe((result)=>{
+      console.warn(result)
+    })
+
+    //window.location.reload();
+    return this.datasToBD;
+  }
+
+  UpdateCompany(nameNew:string,nitNew:string,bussinessNew:string,dateNew:string,attendantNew:string){
+    this.IdCompany=this.companyList[this.index].url;
+    this.datasToBD={
+      "Name":nameNew,
+      "NIT":nitNew,
+      "businessName":bussinessNew,
+      "applicationDate":dateNew,
+      "status":this.statusNew,
+      "attendant":attendantNew
+    }
+    console.log(this.IdCompany)
+
+    this.companyService.setCompanies(this.datasToBD,this.IdCompany).subscribe((result)=>{
+      console.warn(this.IdCompany)
+    })
+    //window.location.reload();
+    return this.datasToBD;
   }
 }
--- src/app/services/companies.service.ts
+++ src/app/services/companies.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompaniesService {
-
+  url='https://djangoorus.herokuapp.com/company/';
   constructor(private http: HttpClient) {
     console.log('http service')
    }
 
    getCompanies():any{
-     return this.http.get('https://djangoorus.herokuapp.com/company/')
+     return this.http.get(this.url)
+   }
+   addCompanies(data:any){
+    return this.http.post(this.url,data)
+   }
+   setCompanies(data:any,putUrl:string){
+    return this.http.put(putUrl,data)
+   }
+   deleteCompanies(putUrl:string){
+    return this.http.delete(putUrl)
    }
 }
